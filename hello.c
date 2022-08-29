672fb9cded44b454847e26236b6d04626268ab7d
tzhao0329 <111652431+tzhao0329@users.noreply.github.com>
2022-08-29 15:19:33 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tingting Zhao
+ * Date: 2022/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tingting Zhao\n");
+  printf("Major: Mathematics! \n");
+  
+  return 0;
+}
